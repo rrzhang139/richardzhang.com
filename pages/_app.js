21604351702374d48b6e8b7a2bdbb0645e3b8eae
Can Toraman <toraman@usc.edu>
2021-02-07 11:07:53 +0300

    seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,26 @@
 import "../styles/globals.css";
+import Head from "next/head";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Can Toraman</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="Can Toraman, computer science student @ University of Southern California. Intrested in education technologies, AI for autonomy, creative web development."
+        ></meta>
+        <meta
+          property="og:description"
+          content="Can Toraman, computer science student @ University of Southern California. Intrested in education technologies, AI for autonomy, creative web development."
+          key="ogdesc"
+        />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,8 @@
-import Head from "next/head";
 import Intro from "../components/Intro";
-import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Can Toraman</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <div className="h-screen flex flex-col justify-center">
         <Intro />
       </div>
