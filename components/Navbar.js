1aeb2844219acfef1b51d8d67b2ef7783007f93f
Can Toraman <toraman@usc.edu>
2021-09-22 15:00:19 -0700

    update resume

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,36 +3,36 @@ import Link from "next/link";
 const Navbar = () => {
   const svgSize = 35;
   return (
-    <nav className="m-auto w-auto sm:w-max p-4 font-yusei border-black border-b-8 border-opacity-10 mb-10">
-      <Link href="/">
+    <nav className='m-auto w-auto sm:w-max p-4 font-yusei border-black border-b-8 border-opacity-10 mb-10'>
+      <Link href='/'>
         <a>
-          <h1 className="text-4xl text-center mb-5">Can Toraman</h1>
+          <h1 className='text-4xl text-center mb-5'>Can Toraman</h1>
         </a>
       </Link>
 
-      <ul className="flex xs:flex-wrap xs:justify-center xs:text-center">
-        <li className="mx-4 xs:w-1/3 xs:py-3 md:py-0 md:w-auto">
-          <Link href="/about">
+      <ul className='flex xs:flex-wrap xs:justify-center xs:text-center'>
+        <li className='mx-4 xs:w-1/3 xs:py-3 md:py-0 md:w-auto'>
+          <Link href='/about'>
             <a>
               About <span>&rarr;</span>
             </a>
           </Link>
         </li>
-        <li className="mx-4 xs:w-1/3 xs:py-3 md:py-0 md:w-auto">
-          <Link href="/projects">
+        <li className='mx-4 xs:w-1/3 xs:py-3 md:py-0 md:w-auto'>
+          <Link href='/projects'>
             <a>
               Projects <span>&rarr;</span>
             </a>
           </Link>
         </li>
-        <li className="mx-4 xs:w-1/3 md:w-auto">
-          <a href="/assets/Resume.pdf" download="Can Toraman Resume.pdf">
+        <li className='mx-4 xs:w-1/3 md:w-auto'>
+          <a href='/assets/Can-Toraman-Resume.pdf' download='Can Resume.pdf'>
             Resume
             <span>&rarr;</span>
           </a>
         </li>
-        <li className="mx-4 xs:w-1/3 md:w-auto">
-          <Link href="/articles">
+        <li className='mx-4 xs:w-1/3 md:w-auto'>
+          <Link href='/articles'>
             <a>
               Articles <span>&rarr;</span>
             </a>
