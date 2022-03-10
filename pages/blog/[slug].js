901e11e9f2f34e0330124f160cb2e295f38d5209
Can Toraman <toraman@usc.edu>
2022-03-10 12:30:36 -0800

    blog content

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -11,15 +11,28 @@ const components = { Button, SyntaxHighlighter };
 
 const PostPage = ({ frontMatter: { title, date, description }, mdxSource }) => {
   return (
-    <article className='prose px-5 md:px-0 max-w-2xl mx-auto my-6 md:my-20'>
-      <div className='mt-4'>
-        <h1 className=''>{title}</h1>
-        <p className='-mt-6 italic'>{description}</p>
-        <section className='mt-10'>
-          <MDXRemote {...mdxSource} components={components} />
-        </section>
+    <section>
+      {/* header section */}
+      <div className='md:border-b border-stone-300'>
+        <div className='px-4 max-w-2xl mx-auto md:px-0'>
+          <div className='margin comes here... for_<sm: my-12 for_medium: md:my-28 for_large: lg:my-32'>
+            <h1 className='global: text-stone-700 font-serif for_<sm: text-4xl for_medium: md:text-5xl for_large: lg:text-7xl lg:leading-snug '>
+              {title}
+            </h1>
+            <h5 className='text-stone-500  text-sm for large: my-2'>{date}</h5>
+            <h4 className='text-stone-500 font-bold for large: my-2'>
+              {description}
+            </h4>
+          </div>
+        </div>
       </div>
-    </article>
+      {/* blog content section */}
+      <div className='px-4 max-w-2xl lg:max-w-4xl mx-auto md:px-0 my-12 md:my-28 lg:my-32'>
+        <article className='prose md:my-40 md:px-8 lg:px-0 max-w-5xl'>
+          <MDXRemote {...mdxSource} components={components} />
+        </article>
+      </div>
+    </section>
   );
 };
 
