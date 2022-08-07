1a2669c2f58cbc8b852c2b2d797258bea0693ecc
Can Toraman <toraman@usc.edu>
2022-08-06 22:02:29 -0700

    server error

---
+++ pages/500.js
@@ -0,0 +1,19 @@
+import LandingContainer from "../components/Containers/LandingContainer";
+import Link from "next/link";
+
+const ServerError = () => {
+  return (
+    <LandingContainer classnames="text-center mt-40 space-y-2">
+      <p>Oops, maybe someting went wrong?</p>
+      <Link href="/">
+        <p>
+          <span className="text-blue-400 hover:cursor-pointer">
+            Take me back to homepage
+          </span>
+        </p>
+      </Link>
+    </LandingContainer>
+  );
+};
+
+export default ServerError;
