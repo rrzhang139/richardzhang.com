46f91d3f1ddd7f9b03f53c419bd8b16ec7a9e8b9
Can Toraman <toraman@usc.edu>
2022-08-09 14:47:15 -0700

    projects

--- components/Nav.js
+++ components/Nav.js
@@ -9,9 +9,9 @@ const Nav = () => {
   }, []);
 
   return (
-    <nav className="block pt-5 max-w-xl mx-auto">
+    <nav className="block max-w-xl pt-5 mx-auto">
       <ul
-        className="flex space-x-3 p-4 justify-start sm:justify-end"
+        className="flex justify-start p-4 space-x-3 sm:justify-end"
         style={{
           fontFamily: "Source Serif Pro, serif",
         }}
@@ -24,6 +24,16 @@ const Nav = () => {
             onClick={() => setPath("")}
           >
             home
+          </h4>
+        </Link>
+        <Link href="/projects">
+          <h4
+            className={`text-sm hover:cursor-pointer ${
+              path === "projects" && "border-b border-black"
+            }`}
+            onClick={() => setPath("projects")}
+          >
+            projects
           </h4>
         </Link>
         <Link href="/writing">
---
+++ pages/projects/index.js
@@ -0,0 +1,57 @@
+import LandingContainer from "../../components/Containers/LandingContainer";
+import Link from "next/link";
+
+const projects = [
+  {
+    title: "Study group app for USC students",
+    description: "USC's premier study group management app for its students.",
+    url: "https://www.studyonusc.com",
+    slug: "study-on-usc",
+  },
+  {
+    title: "Study group app for USC students",
+    description: "USC's premier study group management app for its students.",
+    url: "https://www.studyonusc.com",
+    slug: "study-on-usc",
+  },
+];
+
+const Projects = () => {
+  return (
+    <LandingContainer classnames="mt-12 space-y-16">
+      <div>
+        <h1 className="text-2xl">Projects</h1>
+        <p className="mt-4 italic">
+          My production projects. These are not open source.
+        </p>
+      </div>
+      <ul className="space-y-12">
+        {projects.map((project, index) => (
+          <li key={index} className="block border-gray-300">
+            <div className="max-w-xl space-y-1">
+              <div>
+                <h3 className="text-base">{project.title}</h3>
+              </div>
+              <p>
+                <a href={project.url} target="_blank" rel="noopener noreferrer">
+                  <span className="text-xs hover:cursor-pointer">
+                    {project.url.split("www.")[1].split(".com")[0] + ".com"}
+                  </span>
+                </a>
+              </p>
+              <p>
+                <Link href={`/projects/${project.slug}`}>
+                  <span className="text-xs hover:cursor-pointer">
+                    Read details...
+                  </span>
+                </Link>
+              </p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </LandingContainer>
+  );
+};
+
+export default Projects;
