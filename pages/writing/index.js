ded221eba53372aac9915a9077e5d45b3f514a8d
Can Toraman <toraman@usc.edu>
2022-11-25 18:39:27 -0800

    release

--- pages/writing/index.js
+++ pages/writing/index.js
@@ -1,33 +1,49 @@
-import Nav from "../../components/Nav";
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import Link from "next/link";
 import Container from "../../components/Container/Container";
+import Head from "next/head";
 
 const Home = ({ posts }) => {
   return (
-    <Container title="Writing">
-      <ul className="">
-        {posts.map((post, index) => (
-          <li
-            key={index}
-            className="font-text text-sm text-stone-400 hover:text-white transition-colors ease-in duration-300 hover:cursor-pointer py-3 border-b border-b-neutral-700 hover:border-b-neutral-500"
-          >
-            <Link
-              href={`writing/${post.frontMatter.slug}`}
-              passHref
-              className="hover:cursor-pointer"
+    <>
+      <Head>
+        <title>Writing | Can Toraman</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="Opinions on philosophical and technical aspects of the software industry."
+        ></meta>
+        <meta
+          property="og:description"
+          content="Opinions on philosophical and technical aspects of the software industry."
+          key="ogdesc"
+        />
+      </Head>
+
+      <Container title="Writing">
+        <ul className="">
+          {posts.map((post, index) => (
+            <li
+              key={index}
+              className="font-text text-sm text-stone-400 hover:text-white transition-colors ease-in duration-300 hover:cursor-pointer py-3 border-b border-b-neutral-700 hover:border-b-neutral-500"
             >
-              <div className="flex justify-between">
-                <h3 className="w-fit">{post.frontMatter.title}</h3>
-                <p>{post.frontMatter.date}</p>
-              </div>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </Container>
+              <Link
+                href={`writing/${post.frontMatter.slug}`}
+                passHref
+                className="hover:cursor-pointer"
+              >
+                <div className="flex justify-between">
+                  <h3 className="w-fit">{post.frontMatter.title}</h3>
+                  <p>{post.frontMatter.date}</p>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </Container>
+    </>
   );
 };
 
